4fab11374a7794f1ebfec8a74bca9c0978da34bd
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-11-26 21:20:59 +0500

    Orb added that is a little animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import styled from "styled-components";
+import bg from "./img/bg.png";
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./components/Orb/Orb";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <AppStyled bg={bg} className="App">
+      <Orb />
+      <MainLayout>
+        <h1>    Hello</h1>
+      </MainLayout>
+    </AppStyled>
   );
 }
-
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg})
+`;
 export default App;

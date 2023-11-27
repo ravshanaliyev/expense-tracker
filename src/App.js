9d8dc178389da6ee88f67f20aef98a408eb5d29d
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-11-27 13:51:27 +0500

    Project is successfully ended

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
+import React, { useState, useMemo } from "react";
 import styled from "styled-components";
 import bg from "./img/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./components/Orb/Orb";
+import Navigation from "./components/Navigation/Navigation";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Income from "./components/Income/Income";
+import Expenses from "./components/Expenses/Expenses";
+import { useGlobalContext } from "./context/globalContext";
+
 function App() {
+  const [active, setActive] = useState(1);
+
+  const global = useGlobalContext();
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
+
   return (
     <AppStyled bg={bg} className="App">
-      <Orb />
+      {orbMemo}
       <MainLayout>
-        <h1>    Hello</h1>
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   );
 }
+
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg})
+  background-image: url(${(props) => props.bg});
+  position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyle';
+import { GlobalProvider } from './context/globalContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle /> 
-    <App />
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
